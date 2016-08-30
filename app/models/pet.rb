bf649b7d0e2ffc7aeaf4b705f4bfc18691c0512c
Joshua Peters <jpeters280@gmail.com>
2016-08-30 11:41:59 -0700

    added image uploding

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ActiveRecord::Base
+
+  attr_accessible :name, :image, :remote_image_url
   belongs_to :animal
   belongs_to :owner
   has_many :watches
-
+  mount_uploader :image, ImageUploader
 end
