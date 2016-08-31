811ab6517fd7171798d0c7a4715cd740b7a99e34
shivbhardwaj <sbhardwaj192@gmail.com>
2016-08-31 16:11:52 -0700

    added logout and login functionality

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -18,25 +18,36 @@ class SittersController < ApplicationController
   end
 
   def create
-    @sitter=Sitter.create(sitter_params)
-    puts "ths is at sitter", @sitter.first_name
-    if @sitter
-      session[:sitter_id]=@sitter.id
-      @preference=params[:preference]
-      puts @sitter, "This is the @sitter"
-
-      @new_sitter=Sitter.last
-      puts @new_sitter, 'this is new sitter'
-      puts @preference, 'this is @pref'
-      @preference.each do |ani|
-        Prefence.create(sitter_id: @new_sitter.id, animal_id: ani.to_i)
+    sitter = Sitter.new(sitter_params)
+    if sitter.valid?
+      sitter.save
+      session[:sitter_id]=sitter.id
+      preference=params[:preference]
+      new_sitter=Sitter.last
+      preference.each do |ani|
+        Prefence.create(sitter_id: new_sitter.id, animal_id: ani.to_i)
       end
-      redirect_to "/sitters/#{@sitter.id}"
+      redirect_to "/sitters/#{sitter.id}"
     else
-      flash[:errors]=@sitter.errors.full_messages
+      flash[:errors]=sitter.errors.full_messages
       redirect_to "/"
     end
   end
+  #   @sitter=Sitter.create(sitter_params)
+  #   puts "ths is at sitter", @sitter.first_name
+  #   if @sitter
+  #     session[:sitter_id]=@sitter.id
+  #     @preference=params[:preference]
+  #     @new_sitter=Sitter.last
+  #     @preference.each do |ani|
+  #       Prefence.create(sitter_id: @new_sitter.id, animal_id: ani.to_i)
+  #     end
+  #     redirect_to "/sitters/#{@sitter.id}"
+  #   else
+  #     flash[:errors]=@sitter.errors.full_messages
+  #     redirect_to "/"
+  #   end
+  # end
   #sitter dashboard to view/accept proposals
   def show
     @sitter=Sitter.find(session[:sitter_id])
