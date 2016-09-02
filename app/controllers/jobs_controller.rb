c512487d18edc8cfb83f4ff3c7b0869fa0c0712b
Joshua Peters <jpeters280@gmail.com>
2016-09-02 10:06:25 -0700

    sorry shawn not the last one

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,17 @@
 class JobsController < ApplicationController
   def create
     job=Job.create(job_params)
-    #twiliooooooooooooooooo
-    account_sid="AC2ee7c1f679be05f26f9c89866ff35fa1"
-    auth_token=ENV["TWILIO_AUTH_TOKEN"]
-    client = Twilio::REST::Client.new account_sid, auth_token
-    from = "+14087054722"
+    # #twiliooooooooooooooooo
+    # account_sid="AC2ee7c1f679be05f26f9c89866ff35fa1"
+    # auth_token=ENV["TWILIO_AUTH_TOKEN"]
+    # client = Twilio::REST::Client.new account_sid, auth_token
+    # from = "+14087054722"
 
-    client.account.messages.create(
-      :from => from, 
-      :to => job.sitter.phone, 
-      :body => "Hey, #{job.sitter.first_name}, you just scored a new gig through SitterCritter.com!"
-    )
+    # client.account.messages.create(
+    #   :from => from, 
+    #   :to => job.sitter.phone, 
+    #   :body => "Hey, #{job.sitter.first_name}, you just scored a new gig through SitterCritter.com!"
+    # )
     watch=Watch.where(:watchable_id=>params[:proposal_id], :watchable_type=>'newproposal')
     watch.each do |w|
     	w.update(:watchable_id=>job.id, :watchable_type=>'newjob')
