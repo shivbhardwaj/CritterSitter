120c895ce815ebfdbc083859ad2d8339bead4713
shivbhardwaj <sbhardwaj192@gmail.com>
2016-09-15 15:32:56 -0700

    This is the application

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -7,28 +7,28 @@ class ProposalsController < ApplicationController
       flash[:success] = ["Proposal has been created & sent out!"]
       newproposal= Proposal.last
 
-      # #twiliooooooooooooooooo
-      account_sid="AC2ee7c1f679be05f26f9c89866ff35fa1"
-      auth_token=ENV["TWILIO_AUTH_TOKEN"]
-      client = Twilio::REST::Client.new account_sid, auth_token
-      from = "+14087054722"
+      # # #twiliooooooooooooooooo
+      # account_sid="AC2ee7c1f679be05f26f9c89866ff35fa1"
+      # auth_token=ENV["TWILIO_AUTH_TOKEN"]
+      # client = Twilio::REST::Client.new account_sid, auth_token
+      # from = "+14087054722"
 
-      @send_sitters={}
-      @sitters=Sitter.all
-      @sitters.each do |s|
-        @check_dist=Geocoder::Calculations.distance_between(Geocoder.coordinates(newproposal.zip.to_i), Geocoder.coordinates(s.zip.to_i))
-        if (@check_dist<25)
-          @send_sitters[s.phone]=s.first_name         
-        end
-        @send_sitters.each do |key, value|
-          client.account.messages.create(
-            :from => from, 
-            :to => key, 
-            :body => "Hey, #{value}, check out the newest opportunity on SitterCritter.com!"
-          )
-        end
-      end
-      #end twilioooooo
+      # @send_sitters={}
+      # @sitters=Sitter.all
+      # @sitters.each do |s|
+      #   @check_dist=Geocoder::Calculations.distance_between(Geocoder.coordinates(newproposal.zip.to_i), Geocoder.coordinates(s.zip.to_i))
+      #   if (@check_dist<25)
+      #     @send_sitters[s.phone]=s.first_name         
+      #   end
+      #   @send_sitters.each do |key, value|
+      #     client.account.messages.create(
+      #       :from => from, 
+      #       :to => key, 
+      #       :body => "Hey, #{value}, check out the newest opportunity on SitterCritter.com!"
+      #     )
+      #   end
+      # end
+      # #end twilioooooo
  	    params[:petid].each do |p|
  		     Watch.create(pet_id: p.to_i, watchable_id: newproposal.id, watchable_type: "newproposal")
  	    end
